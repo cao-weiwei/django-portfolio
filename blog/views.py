2b5a73c0ee7fc557b98f69310ce18659877f11e7
weiwei.cao <weiwei.cao.cn@gmail.com>
2021-01-30 21:38:40 -0500

    decomposing entire home html into base.html and index.html and adding some testing data

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render, HttpResponse
+from blog.models import BlogPost
+
+
+def base(request):
+    """
+    For rendering the base.html, includes the header, footer and right-side nav.
+    - Header includes Website title, Nav links
+    - Footer includes Copyright and other information
+    - Right-side nav includes
+    """
+    context = {
+        'website_title': 'White & Black',
+    }
+
+    return render(request, 'blog/base.html', context)
 
 
 def index(request):
-    return render(request, 'blog/base.html', {'website_title': 'White & Black'})
+    """
+    This function fetch all blog posts and render the index.html which is the main region of base.html
+    """
+    blog_posts = BlogPost.objects.all()
+
+    context = {
+        'website_title': 'White & Black',
+        'blog_posts': blog_posts,
+    }
+
+    return render(request, 'blog/index.html', context)
